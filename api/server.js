b86e9634daedd4cba2a1c3f520ff2975b62359ed
Trey <treywroberts@gmail.com>
2021-03-11 18:15:02 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -1,16 +1,18 @@
 // implement your server here
 // require your posts router and connect it here
-require('dotenv').config()
+// require('dotenv').config()
 const express = require('express');
 
 const postRouter = require('./posts/posts-router.js')
 
 const server = express();
 server.use(express.json())
+
+
 server.use('/api/posts', postRouter)
-if (process.env.NODE_ENV === 'development') {
-    const cors = require('cors')
-    server.use(cors())
-}
+// if (process.env.NODE_ENV === 'development') {
+//     const cors = require('cors')
+//     server.use(cors())
+// }
 
 module.exports = server;
